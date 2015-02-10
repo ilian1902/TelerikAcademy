c70b987be281c82cf6fbd8791d32aa799df6b00a
Ilian Bratanov <ilian.bratanov82@gmail.cm>
2015-02-10 21:04:01 +0200

    Add

--- C#Part2 - Homework/Arrays - Homework/10.FindSumInArray/FindSumInArray.cs
+++ C#Part2 - Homework/Arrays - Homework/10.FindSumInArray/FindSumInArray.cs
@@ -14,7 +14,53 @@
          */
         static void Main()
         {
-            
+            Console.Write("Enter size of array = ");
+            int size = int.Parse(Console.ReadLine());
+            int[] userArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Index [{0}] = ", i);
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter S = ");
+            int s = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int bigSum = 0;
+            int countIndex = 0;
+            int finelSum;
+
+
+            for (int i = 1; i < size; i++)
+            {
+                int currentIndex = userArray[i - 1];
+                int nextIndex = userArray[i];
+                sum = currentIndex;
+
+                if (sum < s)
+                {
+                    sum += nextIndex;
+                    bigSum += sum;
+                    if (bigSum == s)
+                    {
+                        bigSum = sum;
+                        break;
+                    }
+                    bigSum = userArray[i - 1] + userArray[i];
+                }
+                if (sum > s)
+                {
+                    countIndex = 0;
+                }
+                countIndex++;
+
+            }
+            for (int i = 0; i < countIndex; i++)
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine(bigSum);
         }
     }
 }
---
+++ C#Part2 - Homework/Arrays - Homework/11.BinarySearch/BinarySearch.cs
@@ -0,0 +1,53 @@
+﻿namespace BinarySearch
+{
+    using System;
+
+    class BinarySearch
+    {
+        // Write a program that finds the index of given element 
+        // in a sorted array of integers by using the Binary search algorithm.
+
+        static void Main()
+        {
+            Console.Write("Enter size of array = ");
+            int size = int.Parse(Console.ReadLine());
+            int[] userArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Index [{0}]", i);
+                userArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Enter number which is sought = ");
+            int searchNum = int.Parse(Console.ReadLine());
+
+            Array.Sort(userArray);
+
+            int searchIndex = BinarySearch(userArray, searchNum, 0, userArray.Length);
+            Console.WriteLine(searchIndex);
+        }
+        private static int BinarySearch(int[] someArray, int number, int min, int max)
+        {
+            if (max < min)
+            {
+                return -1;
+            }
+            else
+            {
+                int middleIndex = (min + max) / 2;
+                if (someArray[middleIndex] > number)
+                {
+                    return BinarySearch(someArray, number, min, middleIndex - 1);
+                }
+                else if (someArray[middleIndex] < number)
+                {
+                    return BinarySearch(someArray, number, middleIndex + 1, max);
+                }
+                else
+                {
+                    return middleIndex;
+                }
+            }
+        }
+    }
+}
