7f38c8c92bfa4a2dd5f24764aabb941a9b785042
Ilian Bratanov <ilian.bratanov82@gmail.cm>
2015-03-03 00:14:28 +0200

    Add homework

---
+++ C#Part2 - Homework/07. Exception Handling - Homework/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿namespace SquareRoot
+{
+    using System;
+   
+    class SquareRoot
+    {
+        /* Write a program that reads an integer number and calculates and prints its square root.
+           If the number is invalid or negative, print Invalid number.
+           In all cases finally print Good bye.
+           Use try-catch-finally block.
+         */
+
+        static void Main()
+        {
+            Console.WriteLine("Enter number");
+
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalit number!{0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+
+        }
+    }
+}
---
+++ C#Part2 - Homework/07. Exception Handling - Homework/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,46 @@
+﻿namespace EnterNumbers
+{
+    using System;
+
+    class EnterNumbers
+    {
+        /* 
+    Write a method ReadNumber(int start, int end) that enters an integer number in a given range [start…end].
+        If an invalid number or non-number text is entered, the method should throw an exception.
+    Using this method write a program that enters 10 numbers: a1, a2, … a10, such that 1 < a1 < … < a10 < 100
+         */
+
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter start number: ");
+                int start = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter end number: ");
+                int end = int.Parse(Console.ReadLine());
+                ReadNumber(start, end);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch (ArgumentOutOfRangeException ie)
+            {
+                Console.WriteLine("Error " + ie.Message);
+            }
+        }
+
+        static void ReadNumber(int start, int end)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter number > start and number < end");
+                int numb = int.Parse(Console.ReadLine());
+                if (numb < start || numb > end)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
